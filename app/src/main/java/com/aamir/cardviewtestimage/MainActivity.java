2f3200ac5ae07b872a75b6c1ad4d9a3f89e7a478
malik <aamirmalik00116@gmail.com>
2020-04-09 06:05:49 -0500

    first

---
+++ app/src/main/java/com/aamir/cardviewtestimage/MainActivity.java
@@ -0,0 +1,59 @@
+package com.aamir.cardviewtestimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.annotation.SuppressLint;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.luseen.spacenavigation.SpaceItem;
+import com.luseen.spacenavigation.SpaceNavigationView;
+import com.luseen.spacenavigation.SpaceOnClickListener;
+
+public class MainActivity extends AppCompatActivity {
+    LinearLayout ln;
+
+    @SuppressLint("WrongViewCast")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ln=findViewById(R.id.mylayout);
+
+        AnimationDrawable animationDrawable= (AnimationDrawable) ln.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+
+        SpaceNavigationView spaceNavigationView = (SpaceNavigationView) findViewById(R.id.space);
+        spaceNavigationView.initWithSaveInstanceState(savedInstanceState);
+        spaceNavigationView.addSpaceItem(new SpaceItem("", R.drawable.ic_favorite_black_30dp));
+        spaceNavigationView.addSpaceItem(new SpaceItem("", R.drawable.ic_person_black_30dp));
+        spaceNavigationView.addSpaceItem(new SpaceItem("", R.drawable.ic_cloud_black_30dp));
+        spaceNavigationView.addSpaceItem(new SpaceItem("", R.drawable.ic_settings_applications_black_30dp));
+
+
+
+        spaceNavigationView.setSpaceOnClickListener(new SpaceOnClickListener() {
+            @Override
+            public void onCentreButtonClick() {
+                Toast.makeText(MainActivity.this,"onCentreButtonClick", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemClick(int itemIndex, String itemName) {
+                Toast.makeText(MainActivity.this, itemIndex + " " + itemName, Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onItemReselected(int itemIndex, String itemName) {
+                Toast.makeText(MainActivity.this, itemIndex + " " + itemName, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+    }
+}
